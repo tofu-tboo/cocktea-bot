9c0e159e741afa8f1ac496f639cc11e3200dd424
DGIST2023choidoyun <cdydoyun@dgist.ac.kr>
2025-09-16 00:05:30 +0900

    admin message handling

--- Bots/cocktea/cocktea.js
+++ Bots/cocktea/cocktea.js
@@ -61,8 +61,8 @@ function onMessage(msg) {
 
         if (res.succeed) {
           botReply(reply, "업데이트 완료.");
-          if (res.admin_message) {
-            botReply(reply, res.admin_message);
+          if (json.admin_message) {
+            botReply(reply, json.admin_message);
           }
         } else {
           var message = json.message;
